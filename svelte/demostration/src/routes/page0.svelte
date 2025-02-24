ea90d4cc1f7edc286e85fe71146443df694ac1f9
mrAnderson2159 <there.is.no.spoon.2159@gmail.com>
2025-02-24 10:39:49 +0100

    svelte proj

---
+++ svelte/demostration/src/routes/page0.svelte
@@ -0,0 +1,31 @@
+<script lang='ts'>
+    function onclick() {
+        console.log('The button has been click from this ' +
+        'clear defined function in the script tag');
+    }
+
+    let number = 5;
+</script>
+
+<button {onclick}>
+    Click me!
+</button>
+
+<h1>{number}</h1>
+<p>Hello!</p>
+<div class="container">
+    <p>This is the left side</p>
+    <p>This is the right side</p>
+</div>
+
+<style>
+    h1 {
+        color: blue;
+        font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+    }
+
+    .container {
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
