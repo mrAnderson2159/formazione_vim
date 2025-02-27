4ccc5cc73c766a1f983e32c07b7c3240d1ed61ae
V. Molinari - mac.mini <there.is.no.spoon.2159@gmail.com>
2025-02-27 22:27:46 +0100

    flutter

--- flutter/first_app/lib/main.dart
+++ flutter/first_app/lib/main.dart
@@ -1,29 +1,32 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            // Add a gradient background
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Color.fromARGB(220, 26, 2, 80),
-                Color.fromARGB(255, 45, 7, 98),
-              ],
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello World!',
-              style: TextStyle(fontSize: 28, color: Colors.white),
-            ),
-          ),
+  runApp(const MaterialApp(home: Scaffold(body: GradientContainer())));
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        // Add a gradient background
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Color.fromARGB(220, 26, 2, 80),
+            Color.fromARGB(255, 45, 7, 98),
+          ],
         ),
       ),
-    ),
-  );
+      child: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(fontSize: 28, color: Colors.white),
+        ),
+      ),
+    );
+  }
 }
