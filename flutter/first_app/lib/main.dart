4ac3e51142a975597293f0a44e6996bcf926134d
V. Molinari - mac.mini <there.is.no.spoon.2159@gmail.com>
2025-02-27 21:44:42 +0100

    flutter

--- flutter/first_app/lib/main.dart
+++ flutter/first_app/lib/main.dart
@@ -1,5 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text('Hello World!')));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color.fromARGB(220, 26, 2, 80),
+                Color.fromARGB(255, 45, 7, 98),
+              ],
+            ),
+          ),
+          child: const Center(child: Text('Hello World!')),
+        ),
+      ),
+    ),
+  );
 }
