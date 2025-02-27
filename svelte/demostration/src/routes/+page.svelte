69b7b28a10f03f91e55872981b2965e564a3c63d
mrAnderson2159 <there.is.no.spoon.2159@gmail.com>
2025-02-27 11:20:35 +0100

    svelte componenti

--- svelte/demostration/src/routes/+page.svelte
+++ svelte/demostration/src/routes/+page.svelte
@@ -1,42 +1,12 @@
-<script lang='ts'>
-    let number = $state(0);
-    let userInformation = $derived.by(() => calculateUserInfo(number));
-    function onclick() {
-        number++;
-    }
-    function calculateUserInfo(number: number) {
-        switch (number) {
-            case 0:
-                return "Hey! Why don't you try to click the button";
-            case 1:
-                return "You clicked exactly one time";
-            default:
-                return `You clicked ${number} times`;
-        }
-    }
+<script>
+    import UserInput from "$lib/components/userInput.svelte";
 </script>
 
-<button {onclick}>
-    Click me!
-</button>
-
-<h1>{number}</h1>
-<p>{userInformation}</p>
-
-<p>Hello!</p>
-<div class="container">
-    <p>This is the left side</p>
-    <p>This is the right side</p>
-</div>
+<UserInput username={"mario"}>Messaggio scritto dalla componente</UserInput>
 
 <style>
     h1 {
         color: blue;
         font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
-    }
-
-    .container {
-        display: flex;
-        justify-content: space-between;
     }
 </style>
