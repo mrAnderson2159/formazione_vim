5cb420070bf5a06ced83e8845da6ae0db370149e
V. Molinari - mac.mini <there.is.no.spoon.2159@gmail.com>
2025-03-01 11:16:10 +0100

    conditional rendering

--- svelte/demostration/src/routes/+page.svelte
+++ svelte/demostration/src/routes/+page.svelte
@@ -1,12 +1,37 @@
 <script lang="ts">
     let userName = $state('Valerio');
+    let isEditMode = $state(false);
 </script>
 
 {#snippet userInput(exempleString: string)}
-    <h1>{exempleString}</h1>
-    <h2>Your username</h2>
-    <input type="text" bind:value={userName} placeholder="Enter your name" />
-    <p>{userName}</p>
+    <div class="container">
+        <h1>{exempleString}</h1>
+        <h2>Your username</h2>
+
+        {#if isEditMode}
+            <div class="input_container">
+                <input type="text" bind:value={userName} placeholder="Enter your name" />
+            </div>
+        {:else}
+            <p>{userName}</p>
+        {/if}
+    </div>
 {/snippet}
 
 {@render userInput('Example 1')}
+
+<button onclick={() => isEditMode = !isEditMode}>
+    {isEditMode ? 'Save changes' : 'Edit fields'}
+</button>
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .input_container {
+        margin-top: 1rem;
+        margin-bottom: 1rem;
+    }
+</style>
---
+++ svelte/demostration/src/routes/legacy/page4.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    let userName = $state('Valerio');
+</script>
+
+{#snippet userInput(exempleString: string)}
+    <h1>{exempleString}</h1>
+    <h2>Your username</h2>
+    <input type="text" bind:value={userName} placeholder="Enter your name" />
+    <p>{userName}</p>
+{/snippet}
+
+{@render userInput('Example 1')}
