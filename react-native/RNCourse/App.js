3f29b02efd9f69791a51781acdcbfc2bda466c60
MacBook - Valerio Molinari <there.is.no.spoon.2159@gmail.com>
2025-03-05 08:54:25 +0100

    flexbox

--- react-native/RNCourse/App.js
+++ react-native/RNCourse/App.js
@@ -28,17 +28,26 @@ export default function App() {
 const styles = StyleSheet.create({
     appContainer: {
         padding: 50,
+        paddingHorizontal: 16,
+        backgroundColor: "white",
+        height: "100%",
     },
     inputContainer: {
         flexDirection: "row",
         justifyContent: "space-between",
+        alignItems: "center", // per correggere il problema del testo in verticale, che però ormai non
+        // si presenta più
+        paddingBottom: 16,
+        borderBottomWidth: 1,
+        borderBottomColor: "#ccc",
+        marginBottom: 16,
     },
     textInput: {
         borderColor: "#ccc",
         borderWidth: 1,
-        padding: 10,
-        width: "80%",
-        marginRight: 10,
+        padding: 8,
+        marginRight: 8,
+        width: "70%",
     },
     button: {
         verticalAlign: "middle",
