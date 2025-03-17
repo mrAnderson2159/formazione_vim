6b272388a13340ea13a0ba70fc5612af164f4620
V. Molinari - mac <there.is.no.spoon.2159@gmail.com>
2025-03-17 18:09:13 +0100

    dice images

--- flutter/first_app/lib/gradient_container.dart
+++ flutter/first_app/lib/gradient_container.dart
@@ -5,23 +5,33 @@ const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer({
+    super.key,
+    required this.colors,
+    required this.text,
+  });
+
+  const GradientContainer.purple({super.key, this.text = 'Purple Flutter!'})
+    : colors = const [
+        Color.fromARGB(220, 26, 2, 80),
+        Color.fromARGB(255, 45, 7, 98),
+      ];
+
+  final List<Color> colors;
+  final String text;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         // Add a gradient background
         gradient: LinearGradient(
           begin: startAlignment,
           end: endAlignment,
-          colors: [
-            Color.fromARGB(220, 26, 2, 80),
-            Color.fromARGB(255, 45, 7, 98),
-          ],
+          colors: colors,
         ),
       ),
-      child: const Center(child: StyledText('Hello World!')),
+      child: Center(child: Image.asset('assets/images/dice-3.png')),
     );
   }
 }
