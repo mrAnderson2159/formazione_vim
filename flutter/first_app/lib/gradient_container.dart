fb603c2d2366287e71fb2c2598180f44e5c335ff
V. Molinari - mac <there.is.no.spoon.2159@gmail.com>
2025-03-17 16:15:08 +0100

    splitting file

---
+++ flutter/first_app/lib/gradient_container.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        // Add a gradient background
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Color.fromARGB(220, 26, 2, 80),
+            Color.fromARGB(255, 45, 7, 98),
+          ],
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(fontSize: 28, color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
