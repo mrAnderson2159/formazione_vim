0f7e0602d334915b6f280dd3785d0a8fa8651527
V. Molinari - mac <there.is.no.spoon.2159@gmail.com>
2025-03-20 16:43:15 +0100

    RN: testing android ripple

--- react-native/guessNumberApp/src/components/elements/PrimaryButton.js
+++ react-native/guessNumberApp/src/components/elements/PrimaryButton.js
@@ -1,16 +1,53 @@
 import { StyleSheet, Pressable, View, Text } from "react-native";
 
 export default function PrimaryButton({ onPress, children }) {
+    /*
+    Il seguente codice non permette di visualizzare l'effetto ripple su Android
+    perché il pressable è all'esterno del view. Per farlo funzionare, bisogna
+    spostare il pressable all'interno del view.
+    
+    <Pressable onPress={onPress} android_ripple={{ color: "#640233" }}>
+        <View style={styles.container}>
+            <Text style={styles.text}>{children}</Text>
+        </View>
+    </Pressable>
+    */
     return (
-        <Pressable onPress={onPress}>
-            <View style={styles.container}>
+        <View style={styles.outerContainer}>
+            <Pressable
+                style={styles.innerContainer}
+                onPress={onPress}
+                android_ripple={{ color: "#640233" }}
+            >
                 <Text style={styles.text}>{children}</Text>
-            </View>
-        </Pressable>
+            </Pressable>
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {},
-    text: {},
+    outerContainer: {
+        borderRadius: 28,
+        margin: 4,
+        overflow: "hidden",
+    },
+    innerContainer: {
+        backgroundColor: "#72063c",
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        elevation: 2,
+    },
+    // container: {
+    //     backgroundColor: "#72063c",
+    //     borderRadius: 28,
+    //     paddingVertical: 8,
+    //     paddingHorizontal: 16,
+    //     elevation: 2,
+    // },
+    text: {
+        color: "white",
+        textAlign: "center",
+        fontSize: 16,
+        fontWeight: "bold",
+    },
 });
--- react-native/guessNumberApp/src/pages/startGamePage.js
+++ react-native/guessNumberApp/src/pages/startGamePage.js
@@ -1,10 +1,30 @@
 import { StyleSheet, TextInput, View, Text } from "react-native";
+import { useState } from "react";
 import PrimaryButton from "../components/elements/PrimaryButton";
 
 export default function StartGamePage() {
+    const [enteredValue, setEnteredValue] = useState("");
+
+    function numberInputHandler(inputText) {
+        setEnteredValue((prevValue) => {
+            if (!/^\d*$/.test(inputText)) return prevValue;
+
+            return inputText;
+        });
+    }
+
     return (
         <View style={styles.container}>
-            <TextInput />
+            <TextInput
+                style={styles.numberInput}
+                maxLength={2}
+                onChangeText={numberInputHandler}
+                value={enteredValue}
+                inputMode="numeric" // iOS only
+                keyboardType="number-pad" // Android only
+                autoCapitalize="none" // Not needed for numbers but just in case
+                autoCorrect={false} // Not needed for numbers but just in case
+            />
             <View style={styles.buttonsContainer}>
                 <PrimaryButton>Reset</PrimaryButton>
                 <PrimaryButton>Confirm</PrimaryButton>
@@ -18,13 +38,29 @@ const styles = StyleSheet.create({
         marginTop: 100,
         marginHorizontal: 24,
         padding: 16,
-        backgroundColor: "#72063c",
+        backgroundColor: "#4e0329",
         borderRadius: 8,
         elevation: 8, // Android only
         shadowColor: "#000", // iOS only
         shadowOffset: { width: 0, height: 2 }, // iOS only
         shadowOpacity: 0.25, // iOS only
         shadowRadius: 6, // iOS only
+        alignItems: "center",
     },
-    buttonsContainer: {},
+    numberInput: {
+        height: 50,
+        width: 50,
+        textAlign: "center",
+        fontSize: 32,
+        borderBottomColor: "#ddb52f",
+        borderBottomWidth: 2,
+        color: "#ddb52f",
+        marginVertical: 8, // top and bottom
+        fontWeight: "bold",
+    },
+    buttonsContainer: {
+        width: "100%",
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
 });
