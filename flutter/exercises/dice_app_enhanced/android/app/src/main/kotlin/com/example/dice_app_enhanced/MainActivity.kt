bb11be8509ab1f84f6bc65bf7d13aec126eddc23
MacBook - Valerio Molinari <there.is.no.spoon.2159@gmail.com>
2025-08-18 19:00:45 +0200

    add flutter/exercises

---
+++ flutter/exercises/dice_app_enhanced/android/app/src/main/kotlin/com/example/dice_app_enhanced/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dice_app_enhanced
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ flutter/exercises/dice_app_enhanced/lib/gradient_container.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  GradientContainer({super.key});
+}
