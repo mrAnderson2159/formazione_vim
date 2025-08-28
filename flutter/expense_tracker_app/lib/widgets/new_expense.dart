18020d36e29695835098a5093bdd7cc62dbc1b93
MacBook - Valerio Molinari <there.is.no.spoon.2159@gmail.com>
2025-08-28 18:04:24 +0200

    Add date picker with showDatePicker, DropdownButton logic and use Navigator.pop to get back

--- flutter/expense_tracker_app/lib/widgets/new_expense.dart
+++ flutter/expense_tracker_app/lib/widgets/new_expense.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker_app/models/expense.dart';
+
+enum _InputFields { title, amount }
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -8,14 +11,40 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
-  final _titleController = TextEditingController();
+  final Map<_InputFields, TextEditingController> _controllers = {
+    _InputFields.title: TextEditingController(),
+    _InputFields.amount: TextEditingController(),
+  };
+
+  DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
+
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    final lastDate = DateTime(now.year, now.month + 1, now.day);
+
+    final DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: firstDate,
+      lastDate: lastDate,
+    );
+
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
 
   // It's VERY IMPORTANT to remember of using dispose when using
   // TextEditingController because otherwise the controller will survive in
   // memory even if the container Widged ceased to exist
   @override
   void dispose() {
-    _titleController.dispose();
+    for (final c in _controllers.values) {
+      c.dispose();
+    }
+
     super.dispose();
   }
 
@@ -28,12 +57,73 @@ class _NewExpenseState extends State<NewExpense> {
           TextField(
             maxLength: 50,
             decoration: const InputDecoration(label: Text('Title')),
-            controller: _titleController,
+            controller: _controllers[_InputFields.title],
           ),
           Row(
             children: [
+              Expanded(
+                child: TextField(
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  decoration: const InputDecoration(
+                    label: Text('Amount'),
+                    prefixText: '€ ',
+                  ),
+                  controller: _controllers[_InputFields.amount],
+                ),
+              ),
+              const SizedBox(width: 16),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      _selectedDate == null
+                          ? 'No date selected'
+                          : formatter.format(_selectedDate!),
+                    ),
+                    IconButton(
+                      onPressed: _presentDatePicker,
+                      icon: const Icon(Icons.calendar_month),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 16),
+          Row(
+            children: [
+              DropdownButton(
+                value: _selectedCategory,
+                items: Category.values
+                    .map(
+                      (category) => DropdownMenuItem(
+                        value: category,
+                        child: Text(category.name.toUpperCase()),
+                      ),
+                    )
+                    .toList(),
+                onChanged: (value) {
+                  if (value == null) {
+                    return;
+                  }
+
+                  setState(() {
+                    _selectedCategory = value;
+                  });
+                },
+              ),
+              const Spacer(),
+              TextButton(
+                onPressed: () {
+                  // This is used to close the modal
+                  Navigator.pop(context);
+                },
+                child: const Text('Cancel'),
+              ),
               ElevatedButton(
-                onPressed: () => print(_titleController.text),
+                onPressed: () => print(_controllers.entries),
                 child: const Text('Save Expense'),
               ),
             ],
