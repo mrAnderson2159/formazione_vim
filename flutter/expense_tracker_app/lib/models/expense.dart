aba272b5441fcb4ee230bda2bd71e360faf0306b
Valerio Molinari <there.is.no.spoon.2159@gmail.com>
2025-10-14 00:22:13 +0800

    Extra: ExpenseBucket using private constructor and factory constructor

--- flutter/expense_tracker_app/lib/models/expense.dart
+++ flutter/expense_tracker_app/lib/models/expense.dart
@@ -38,12 +38,33 @@ class ExpenseBucket {
   final Category category;
   final List<Expense> expenses;
 
-  const ExpenseBucket({required this.category, required this.expenses});
+  const ExpenseBucket._(this.expenses, this.category);
 
-  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
-    : expenses = allExpenses
-          .where((expense) => expense.category == category)
-          .toList();
+  // The previous design of this class exposed a structural weakness
+  // that potentially allowed the creation of an object containing
+  // expenses from different categories, regardless of the chosen one.
+  // As a result, it could incorrectly return the sum of all expenses.
+  //
+  // In this improved version, we use two advanced concepts:
+  // a private constructor and a factory constructor.
+  //
+  // The private constructor encapsulates object creation, preventing
+  // external code from instantiating the class directly.
+  // The factory constructor, on the other hand, is a special kind of
+  // constructor that doesn’t immediately create an instance, but
+  // instead runs some logic before deciding whether and how to do so.
+  // Because no instance exists yet at that point, the `this` keyword
+  // cannot be used inside a factory constructor.
+  //
+  // In this case, we filter the provided list of expenses, just like
+  // in the initializer of the previous example, and then call the
+  // private constructor to return a properly built instance.
+  factory ExpenseBucket.forCategory(List<Expense> allExpenses, category) {
+    final List<Expense> filtered = allExpenses
+        .where((expense) => expense.category == category)
+        .toList();
+    return ExpenseBucket._(filtered, category);
+  }
 
   double get totalExpenses {
     return expenses.fold(0.0, (sum, expense) => sum + expense.amount);
